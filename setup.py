c99b040e50a756e931066e5e857fe281c49aa661
krupin.dv <krupin.dv19@gmail.com>
2015-02-10 17:44:02 +0300

    version and requirements

--- setup.py
+++ setup.py
@@ -16,12 +16,10 @@ setup(
     include_package_data=True,
     zip_safe=False,  # because we're including media that Django needs
     install_requires=[
-        'django',
         'django-annoying',
         'django-picklefield',
         'django-oauth-tokens>=0.4.10',
         'django-m2m-history>=0.2.0',
-        'tweepy',
     ],
     classifiers=[
         'Development Status :: 4 - Beta',
