040a06f5ee0b201bc391f41037b4be1c25b0c426
krupin.dv <krupin.dv19@gmail.com>
2015-03-11 14:10:03 +0300

    fetched and created_at fields check added in tests

--- instagram_api/tests.py
+++ instagram_api/tests.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
+from datetime import datetime
+
 from django.test import TestCase
+from django.utils import timezone
+
 from . models import User, Media, Comment
 #from .factories import UserFactory, MediaFactory
 
@@ -10,20 +14,28 @@ MEDIA_ID = '934625295371059186_205828054'
 
 class UserTest(TestCase):
 
+    def setUp(self):
+        self.time = timezone.now()
+
     def test_fetch_user(self):
         u = User.remote.fetch(USER_ID)
 
         self.assertEqual(int(u.id), USER_ID)
         self.assertEqual(u.username, 'tnt_online')
         self.assertEqual(u.full_name, u'Телеканал ТНТ')
-
         self.assertGreater(len(u.profile_picture), 0)
         self.assertGreater(len(u.website), 0)
+
         self.assertGreater(u.followers_count, 0)
         self.assertGreater(u.media_count, 0)
 
+        self.assertGreater(u.fetched, self.time)
+
 
 class MediaTest(TestCase):
+
+    def setUp(self):
+        self.time = timezone.now()
 
     def test_fetch_media(self):
         m = Media.remote.fetch(MEDIA_ID)
@@ -31,8 +43,12 @@ class MediaTest(TestCase):
         self.assertEqual(m.id, MEDIA_ID)
         #self.assertGreater(len(m.caption), 0)
         self.assertGreater(len(m.link), 0)
+
         self.assertGreater(m.comment_count, 0)
         self.assertGreater(m.like_count, 0)
+
+        self.assertGreater(m.fetched, self.time)
+        self.assertIsInstance(m.created_time, datetime)
 
     def test_fetch_comments(self):
         m = Media.remote.fetch(MEDIA_ID)
@@ -41,6 +57,14 @@ class MediaTest(TestCase):
 
         self.assertGreater(m.comment_count, 0)
         self.assertEqual(m.comment_count, len(comments))
+
+        c = comments[0]
+        self.assertEqual(c.media, m)
+
+        self.assertGreater(len(c.text), 0)
+
+        self.assertGreater(c.fetched, self.time)
+        self.assertIsInstance(c.created_at, datetime)
 
     def test_fetch_likes(self):
         m = Media.remote.fetch(MEDIA_ID)
