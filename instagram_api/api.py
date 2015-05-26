891d243cb93bc3c4d0699c62ebc572eb6bdfc219
ramusus <ramusus@gmail.com>
2015-05-26 23:31:42 +0300

    fixed log_and_raise method

--- instagram_api/api.py
+++ instagram_api/api.py
@@ -41,6 +41,11 @@ class InstagramApi(ApiAbstractBase):
     def get_api_response(self, *args, **kwargs):
         return getattr(self.api, self.method)(*args, **kwargs)
 
+    def log_and_raise(self, e, *args, **kwargs):
+        self.logger.error("Error '%s'. Method %s, args: %s, kwargs: %s, recursion count: %d" % (
+            e, self.method, args, kwargs, self.recursion_count))
+        raise e
+
     def handle_error_code_503(self, e, *args, **kwargs):
         # Rate limited-Your client is making too many request per second
         self.used_access_tokens += [self.api.client_id]
